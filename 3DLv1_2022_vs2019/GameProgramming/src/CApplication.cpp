582d2b17a081ec5b4cea78198bc5f9a97f85a5ce
Saito-Hiromu-Fukuyama <gc22007@anabuki-net.ne.jp>
2022-09-22 13:26:12 +0900

    1終了

--- 3DLv1_2022_vs2019/GameProgramming/src/CApplication.cpp
+++ 3DLv1_2022_vs2019/GameProgramming/src/CApplication.cpp
@@ -1,4 +1,5 @@
 #include "CApplication.h"
+#include "glut.h"
 #include "CRectangle.h"
 
 //クラスのstatic変数
@@ -25,5 +26,38 @@ void CApplication::Start()
 
 void CApplication::Update()
 {
-	
+	//視点の設定
+	//gluLookAt(視点X,視点Y,視点Z 中心X,中心Y,中心Z,上向X,上向Y,上向Z)
+	gluLookAt(1.0f, 2.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+	//描画開始
+	//glBegin(形)
+	//GL_TRIANGLES:三角形
+	glBegin(GL_TRIANGLES);
+
+	//法線(面の向き)の設定
+	//glNormal3f(X座標, Y座標, Z座標)
+	glNormal3f(0.0f, 1.0f, 0.0f);
+
+	//頂点座標の設定
+	//glVertex3f(X座標, Y座標, Z座標)
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, -0.5f);
+
+	//面の向きはZ軸方向
+	glNormal3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-0.5f, 0.0f, 0.0f);
+
+
+	glNormal3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, -0.5f, 0.0);
+
+	//描写終了
+	glEnd();
+
 }
