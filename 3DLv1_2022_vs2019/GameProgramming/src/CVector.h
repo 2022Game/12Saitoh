cf6b76991f0aa2e335d2866207a3195c0d434815
Saito-Hiromu-Fukuyama <gc22007@anabuki-net.ne.jp>
2022-09-26 14:43:10 +0900

    3終了

--- 3DLv1_2022_vs2019/GameProgramming/src/CVector.h
+++ 3DLv1_2022_vs2019/GameProgramming/src/CVector.h
@@ -15,6 +15,11 @@ public:
 	float Y() const;
 	//Zの値を得る
 	float Z() const;
+	//デフォルトコンストラクタ
+	CVector();
+	//コンストラクタ
+	//CVector(X座標, Y座標, Z座標)
+	CVector(float x, float y, float z);
 private:
 	//3D各軸での値を設定
 	float mX, mY, mZ;
