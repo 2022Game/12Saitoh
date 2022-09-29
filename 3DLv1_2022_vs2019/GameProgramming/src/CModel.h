bdceeb7f039bd13b6c5c1333970c6b432d9feb41
Saito-Hiromu-Fukuyama <gc22007@anabuki-net.ne.jp>
2022-09-29 11:35:40 +0900

    7終了

--- 3DLv1_2022_vs2019/GameProgramming/src/CModel.h
+++ 3DLv1_2022_vs2019/GameProgramming/src/CModel.h
@@ -1,3 +1,6 @@
+//vectorのインクルード
+#include<vector>
+#include "CTriangle.h"
 #ifndef CMODEL_H
 #define CMEDEL_H
 /*
@@ -6,7 +9,12 @@
 */
 class CModel
 {
+private:
+	//三角形の可変長配列
+	std::vector<CTriangle> mTriangles;
 public:
+	//描画
+	void Render();
 	//モデルファイルの入力
 	//Load(モデルファイル名, マテリアルファイル名)
 	void Load(char* obj, char* mtl);
