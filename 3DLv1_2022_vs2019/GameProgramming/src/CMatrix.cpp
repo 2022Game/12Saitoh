bd1042d85fe557d24fcd045712879dcce1f46451
Saito-Hiromu-Fukuyama <gc22007@anabuki-net.ne.jp>
2022-10-21 12:29:51 +0900

    課題14終了

--- 3DLv1_2022_vs2019/GameProgramming/src/CMatrix.cpp
+++ 3DLv1_2022_vs2019/GameProgramming/src/CMatrix.cpp
@@ -51,6 +51,7 @@ CMatrix CMatrix::RotateX(float degree)
 	//s—ñ‚ğ•Ô‚·
 	return *this;
 }
+
 void CMatrix::Print()
 {
 	printf("%10f %10f %10f %10f\n",
@@ -105,4 +106,20 @@ CMatrix CMatrix::Scale(float sx, float sy, float sz)
 float CMatrix::M(int r, int c)const
 {
 	return mM[r][c];
+}
+
+//ˆÚ“®s—ñ‚Ìì¬
+//Translate(ˆÚ“®—ÊX,ˆÚ“®—ÊY,ˆÚ“®—ÊZ)
+CMatrix CMatrix::Translate(float mx, float my, float mz)
+{
+	mM[3][0] = mx;
+	mM[3][1] = my;
+	mM[3][2] = mz;
+	//‚±‚Ìs—ñ‚ğ•Ô‚·
+	return *this;
+}
+
+void CMatrix::M(int row, int col, float value)
+{
+	mM[row][col] = value;
 }
