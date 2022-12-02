b25920ba3dec6eab41732c5dc1912d82ff3e3477
Saito-Hiromu-Fukuyama <gc22007@anabuki-net.ne.jp>
2022-12-02 11:47:06 +0900

    課題30終了

--- 3DLv1_2022_vs2019/GameProgramming/src/CPlayer.h
+++ 3DLv1_2022_vs2019/GameProgramming/src/CPlayer.h
@@ -4,7 +4,7 @@
 #include "CCharacter3.h"
 #include "CInput.h"
 #include "CBullet.h"
-
+#include "CColliderLine.h"
 /*
 プレイヤークラス
 キャラクタスを継承
@@ -12,12 +12,15 @@
 class CPlayer : public CCharacter3
 {
 public:
-	CPlayer() {}
+	CPlayer();
 	//CPlayer(位置,回転,スケール)
 	CPlayer(const CVector& pos, const CVector& rot, const CVector& scale);
 	//更新処理
 	void Update();
 private:
+	CColliderLine mLine; //親分コライダ
+	CColliderLine mLine2; //親分コライダ
+	CColliderLine mLine3; //親分コライダ
 	CInput mInput;
 };
 #endif
