ad4641e8220dbbc295a6fb98bdf4c78e0451d038
Saito-Hiromu-Fukuyama <gc22007@anabuki-net.ne.jp>
2023-04-11 12:33:08 +0900

    課題2終了

--- 3DLv2_2023_vs2019_00/3DLv2_2023_vs2019_00/GameProgramming/src/CModelX.h
+++ 3DLv2_2023_vs2019_00/3DLv2_2023_vs2019_00/GameProgramming/src/CModelX.h
@@ -11,8 +11,12 @@ Xファイル形式の3Dモデルデータをプログラムで認識する
 */
 class CModelX {
 private:
+	char* GetToken();  //単語の取り出し
 	char* mpPointer;   //読み込み位置
 	char mToken[1024]; //取り出した単語の領域
+
+	//cが区切り文字ならtrueを返す
+	bool IsDelimiter(char c);
 public:
 	//コンストラクタ
 	CModelX();
