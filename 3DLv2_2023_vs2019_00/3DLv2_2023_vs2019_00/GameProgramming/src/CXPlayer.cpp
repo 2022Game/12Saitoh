349efc4591f3030641bcfbb410b68c434d8ab45d
Saito-Hiromu-Fukuyama <gc22007@anabuki-net.ne.jp>
2023-06-08 14:33:01 +0900

    課題21終了

--- 3DLv2_2023_vs2019_00/3DLv2_2023_vs2019_00/GameProgramming/src/CXPlayer.cpp
+++ 3DLv2_2023_vs2019_00/3DLv2_2023_vs2019_00/GameProgramming/src/CXPlayer.cpp
@@ -2,22 +2,51 @@
 
 void CXPlayer::Update()
 {
-	if (mInput.Key('W'))
+	//移動処理
+	//攻撃モーション中は移動や回転ができないようにする
+	if (mInput.Key('W') &&
+		CXCharacter::AnimationIndex() != 3 &&
+		CXCharacter::AnimationIndex() != 4)
 	{
 		CXCharacter::ChangeAnimation(1, true, 60);
 		mPosition = mPosition + CVector(0.0f, 0.0f, 0.1f) * mMatrixRotate;
 	}
-	else
+	//待機モーション
+	else if (CXCharacter::AnimationIndex() != 3 && 
+		CXCharacter::AnimationIndex() != 4)
 	{
 		CXCharacter::ChangeAnimation(0, true, 60);
 	}
-	if (mInput.Key('A'))
+	if (mInput.Key('A') &&
+		CXCharacter::AnimationIndex() != 3 &&
+		CXCharacter::AnimationIndex() != 4)
 	{
 		mRotation = mRotation + CVector(0.0f, 2.0f, 0.0f);
 	}
-	if (mInput.Key('D'))
+	if (mInput.Key('D') &&
+		CXCharacter::AnimationIndex() != 3 &&
+		CXCharacter::AnimationIndex() != 4)
 	{
 		mRotation = mRotation - CVector(0.0f, 2.0f, 0.0f);
 	}
+	//攻撃処理
+	if (mInput.Key(VK_SPACE) &&
+		CXCharacter::AnimationIndex() != 3 &&
+		CXCharacter::AnimationIndex() != 4)
+	{
+		CXCharacter::ChangeAnimation(3, false, 30);
+	}
+	if (CXCharacter::AnimationIndex() == 3 && 
+		CXCharacter::IsAnimationFinished() == true)
+	{
+		CXCharacter::ChangeAnimation(4, false, 30);
+	}
+	//攻撃モーションが終わったら待機モーションにする
+	if (CXCharacter::AnimationIndex() == 4 &&
+		CXCharacter::IsAnimationFinished() == true)
+	{
+		CXCharacter::ChangeAnimation(0, true, 60);
+	}
+
 	CXCharacter::Update();
 }
