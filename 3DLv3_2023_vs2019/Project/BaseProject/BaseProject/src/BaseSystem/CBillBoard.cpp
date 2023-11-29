53ad8735167886c2faecd5450e0c8d7b7dea8be5
Saito-Hiromu-Fukuyama <gc22007@anabuki-net.ne.jp>
2023-11-29 11:35:44 +0900

    ベースシステム更新

--- 3DLv3_2023_vs2019/Project/BaseProject/BaseProject/src/BaseSystem/CBillBoard.cpp
+++ 3DLv3_2023_vs2019/Project/BaseProject/BaseProject/src/BaseSystem/CBillBoard.cpp
@@ -1,40 +1,38 @@
 #include "CBillBoard.h"
 //#include "CApplication.h"
+#include "CCamera.h"
 
-CBillBoard::CBillBoard()
+CBillBoard::CBillBoard(ETag tag, ETaskPriority prio, int sortOrder, ETaskPauseType pause)
+	: CObjectBase(tag, prio, sortOrder, pause)
 {
 }
 
-CBillBoard::CBillBoard(CVector pos, float w, float h)
+void CBillBoard::SetSize(const CVector2& size)
 {
-	Set(pos, w, h);
-}
-
-CBillBoard::CBillBoard(CVector pos, float w, float h, int priority)
-	: CObjectBase(ETag::eNone, (ETaskPriority)priority)
-{
-	Set(pos, w, h);
-}
-
-void CBillBoard::Set(CVector pos, float w, float h)
-{
-	//位置
-	Position(pos);
-	//大きさの設定
-	Scale(w, h, 1.0f);
 	//三角形の頂点座標設定
-	mT[0].Vertex(CVector(1.0f, 1.0f, 0.0f),
-		CVector(-1.0f, -1.0f, 0.0f), CVector(1.0f, -1.0f, 0.0f));
-	mT[1].Vertex(CVector(-1.0f, 1.0f, 0.0f),
-		CVector(-1.0f, -1.0f, 0.0f), CVector(1.0f, 1.0f, 0.0f));
+	mT[0].Vertex
+	(
+		CVector(size.X(), size.Y(), 0.0f),
+		CVector(-size.X(), -size.Y(), 0.0f),
+		CVector(size.X(), -size.Y(), 0.0f)
+	);
+	mT[1].Vertex
+	(
+		CVector(-size.X(), size.Y(), 0.0f),
+		CVector(-size.X(), -size.Y(), 0.0f),
+		CVector(size.X(), size.Y(), 0.0f)
+	);
 	//法線をZ軸方向
 	mT[0].Normal(CVector(0.0f, 0.0f, 1.0f));
 	mT[1].Normal(CVector(0.0f, 0.0f, 1.0f));
+}
+void CBillBoard::SetColor(const CColor& color)
+{
 	//色を白色を設定
-	mMaterial.Diffuse()[0] = 1.0f;
-	mMaterial.Diffuse()[1] = 1.0f;
-	mMaterial.Diffuse()[2] = 1.0f;
-	mMaterial.Diffuse()[3] = 1.0f;
+	mMaterial.Diffuse()[0] = color.R();
+	mMaterial.Diffuse()[1] = color.G();
+	mMaterial.Diffuse()[2] = color.B();
+	mMaterial.Diffuse()[3] = color.A();
 }
 void CBillBoard::Update()
 {
@@ -46,23 +44,29 @@ void CBillBoard::Render()
 //Render(マテリアルのポインタ)
 void CBillBoard::Render(CMaterial* mpMaterial)
 {
-//	//行列の保存
-//	glPushMatrix();
-//	//拡縮、回転、移動させる
-////	glMultMatrixf((CApplication::ModelViewInverse() *  mMatrix).M());
-//	//ライトオフ
-//	glDisable(GL_LIGHTING);
-//	//描画色の設定
-//	glColor4fv(mpMaterial->Diffuse());
-//	//マテリアル適用
-//	mpMaterial->Enabled();
-//	//三角形の描画
-//	mT[0].Render();
-//	mT[1].Render();
-//	//マテリアル解除
-//	mpMaterial->Disabled();
-//	//ライトオン
-//	glEnable(GL_LIGHTING);
-//	//行列を戻す
-//	glPopMatrix();
+	//行列の保存
+	glPushMatrix();
+
+	//常にカメラの方向を向かせる
+	CCamera* cam = CCamera::CurrentCamera();
+	CMatrix m = cam->GetViewMatrix().Inverse();
+	m.Position(CVector::zero);
+	glMultMatrixf((m * Matrix()).M());
+
+	//ライトオフ
+	glDisable(GL_LIGHTING);
+	//描画色の設定
+	glColor4fv(mpMaterial->Diffuse());
+	//マテリアル適用
+	mpMaterial->Enabled();
+	//三角形の描画
+	mT[0].Render();
+	mT[1].Render();
+	//マテリアル解除
+	mpMaterial->Disabled();
+	//ライトオン
+	glEnable(GL_LIGHTING);
+
+	//行列を戻す
+	glPopMatrix();
 }
