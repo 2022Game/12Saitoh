f906676d7231da66deb864df09d1658ebe2faf1b
Saito-Hiromu-Fukuyama <gc22007@anabuki-net.ne.jp>
2024-01-10 11:20:52 +0900

    敵アイドル関連追加

--- 3DLv3_2023_vs2019/Project/BaseProject/BaseProject/src/Game/CEnemy.cpp
+++ 3DLv3_2023_vs2019/Project/BaseProject/BaseProject/src/Game/CEnemy.cpp
@@ -24,6 +24,7 @@ const CEnemy::AnimData CEnemy::ANIM_DATA[] =
 	{ "",	false,	45.0f,	0.0f	},	// 怯み(左)
 	{ "",	false,	180.0f,	0.0f	},	// 死亡
 	{ "",	true,	135.0f,	0.0f	},	// 飛行
+	{ "",	true,	45.0f,	0.0f	},	// 飛行始め
 	{ "",	false,	183.0f,	0.0f	},	// 飛行中攻撃
 	{ "",	false,	336.0f,	0.0f	},	// 飛行中ブレス
 	{ "",	false,	63.0f,	0.0f	},	// 飛行中怯み
@@ -33,6 +34,8 @@ const CEnemy::AnimData CEnemy::ANIM_DATA[] =
 // コンストラクタ
 CEnemy::CEnemy()
 	: CXCharacter(ETag::eEnemy, ETaskPriority::eEnemy)
+	, mElapsedTime(0.0f)
+	, mIdleTime(0)
 {
 	// インスタンスの設定
 	spInstance = this;
@@ -54,6 +57,7 @@ CEnemy::CEnemy()
 	model->SeparateAnimationSet(0, 535, 550, "Fear_Left");		// 怯み(左)
 	model->SeparateAnimationSet(0, 552, 612, "Death");			// 死亡
 	model->SeparateAnimationSet(0, 662, 707, "Fly");			// 飛行
+	model->SeparateAnimationSet(0, 717, 730, "FlyStart");		// 飛行始め
 	model->SeparateAnimationSet(0, 707, 768, "Fly_Attack");		// 飛行中攻撃
 	model->SeparateAnimationSet(0, 771, 863, "Fly_Breath");		// 飛行中ブレス
 	model->SeparateAnimationSet(0, 885, 906, "Fly_Fear");		// 飛行中怯み
@@ -68,6 +72,9 @@ CEnemy::CEnemy()
 	// ステータスの取得
 	mStatus = ENEMY_STATUS[0];
 
+	// 状態の設定
+	mState = EState::eIdle;
+	// コライダーの生成
 	mpHeadCol = new CColliderSphere(this, ELayer::eDamageCol, 0.2);
 	mpHeadCol->SetCollisionLayers({ ELayer::eAttackCol });
 	mpHeadCol->SetCollisionTags({ ETag::ePlayer });
@@ -116,7 +123,6 @@ void CEnemy::Update()
 
 	CXCharacter::Update();
 	ColliderUpdate();
-
 #ifdef _DEBUG
 	auto enemydata = Instance();
 	CVector enemy = Position();
@@ -131,14 +137,15 @@ void CEnemy::Update()
 	else if (CInput::PushKey('7')) ChangeAnimation(EAnimType::eAttack_Scratching);
 	else if (CInput::PushKey('8')) ChangeAnimation(EAnimType::eAttack_Tail);
 	else if (CInput::PushKey('9')) ChangeAnimation(EAnimType::eRoar);
-	else if (CInput::PushKey('0')) ChangeAnimation(EAnimType::eFear_Right);
-	if (CInput::Key('T') && CInput::PushKey('1')) ChangeAnimation(EAnimType::eFear_Left);
-	else if (CInput::Key('T') && CInput::PushKey('2')) ChangeAnimation(EAnimType::eDeath);
-	else if (CInput::Key('T') && CInput::PushKey('3')) ChangeAnimation(EAnimType::eFly);
-	else if (CInput::Key('T') && CInput::PushKey('4')) ChangeAnimation(EAnimType::eFly_Attack);
-	else if (CInput::Key('T') && CInput::PushKey('5')) ChangeAnimation(EAnimType::eFly_Breath);
-	else if (CInput::Key('T') && CInput::PushKey('6')) ChangeAnimation(EAnimType::eFly_Fear);
-	else if (CInput::Key('T') && CInput::PushKey('7')) ChangeAnimation(EAnimType::eFly_Death);
+	if (CInput::Key('T') && CInput::PushKey('1')) ChangeAnimation(EAnimType::eFear_Right);
+	else if (CInput::Key('T') && CInput::PushKey('2')) ChangeAnimation(EAnimType::eFear_Left);
+	else if (CInput::Key('T') && CInput::PushKey('3')) ChangeAnimation(EAnimType::eDeath);
+	else if (CInput::Key('T') && CInput::PushKey('4')) ChangeAnimation(EAnimType::eFly);
+	else if (CInput::Key('T') && CInput::PushKey('5')) ChangeAnimation(EAnimType::eFly_Attack);
+	else if (CInput::Key('T') && CInput::PushKey('6')) ChangeAnimation(EAnimType::eFly_Breath);
+	else if (CInput::Key('T') && CInput::PushKey('7')) ChangeAnimation(EAnimType::eFly_Fear);
+	else if (CInput::Key('T') && CInput::PushKey('8')) ChangeAnimation(EAnimType::eFly_Death);
+	else if (CInput::Key('T') && CInput::PushKey('9')) ChangeAnimation(EAnimType::eFlyStart);
 #endif
 }
 
--- 3DLv3_2023_vs2019/Project/BaseProject/BaseProject/src/Game/Update_Idle.cpp
+++ 3DLv3_2023_vs2019/Project/BaseProject/BaseProject/src/Game/Update_Idle.cpp
@@ -1,6 +1,21 @@
 #include "CEnemy.h"
+#include "CPlayer.h"
+#include "Time.h"
+#include "Maths.h"
 
 void CEnemy::Update_Idle()
 {
+	if (IsAnimationFinished() &&AnimationIndex()!= (int)EAnimType::eIdle)
+	{
+		ChangeAnimation(EAnimType::eIdle);
+	}
 
+	mElapsedTime += Time::DeltaTime();
+
+	if (mElapsedTime >= 20.0f)
+	{
+		float rand = Math::Rand(0.0f, 10.0f);
+		printf("%.2f\n", rand);
+		mElapsedTime = 0.0f;
+	}
 }
